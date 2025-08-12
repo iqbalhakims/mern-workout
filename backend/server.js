706acb3d9f74f1a9e842b4585d46eed3224869ce
Iqbal Fauzi <teamstac350@gmail.com>
2025-08-12 21:04:09 +0800

    setup db connection with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require ('express')
+const mongoose = require ('mongoose')
 const workoutRoutes = require ('./routes/workouts')
 
 const app = express()
@@ -18,8 +19,17 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
 })
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then (() => {
+    app.listen(process.env.PORT, () => {
+    console.log('Connected to database and listening on PORT', process.env.PORT)
+})
+    })
+    .catch((error) => {
+        console.log(error)
+
+    })
 app.use('api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('listening to port', process.env.PORT)
-})
